7b507eccfa8f5d73f7d427b098eaa0629d773964
BacheyG <cgi.bacsig@gmail.com>
2024-10-15 13:48:10 +0100

    Add normalized and squarelength

--- Vector.hpp
+++ Vector.hpp
@@ -3,10 +3,28 @@
 
 #pragma once
 
+#include <cmath> 
+
 template <typename T>
 struct Vector2D {
 	Vector2D() : X(0), Y(0) {}
 	Vector2D(T x, T y) : X(x), Y(y) {} 
+
+	T SquaredLength() const {
+		return X * X + Y * Y;
+	}	
+	
+	T Length() const {
+		return std::sqrt(SquaredLength());
+	}
+
+	Vector2D Normalized() const {
+		T length = Length();
+		if (length == 0) {
+			return Vector2D(0, 0);
+		}
+		return Vector2D(X / length, Y / length);
+	}
 
 	Vector2D& operator+=(const Vector2D& rhs) {
 		this->X += rhs.X;
@@ -34,7 +52,7 @@ struct Vector2D {
 		return Vector2D(lhs.X * rhs, lhs.Y * rhs);
 	}
 
-	static float squareDistance(const Vector2D& p1, const Vector2D& p2) {
+	static float SquareDistance(const Vector2D& p1, const Vector2D& p2) {
 		float xDifference = (p1.X - p2.X);
 		float yDifference = (p1.Y - p2.Y) 
 		return xDifference * xDifference + yDifference * yDifference;
